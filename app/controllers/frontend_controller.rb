372350ba64da700bc6fb1c4e195430b515f37765
Michael Swan <swanyboy4@gmail.com>
2009-10-03 22:53:34 -0400

    Update

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -2,4 +2,7 @@ class FrontendController <  ApplicationController
   layout "frontend"
   helper Lipsiadmin::View::Helpers::FrontendHelper
   #before_filter :frontend_login_required
+  def index
+    redirect_to :controller => 'frontend/base'
+  end
 end
