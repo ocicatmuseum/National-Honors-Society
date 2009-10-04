76fd260248dc0fb0339701d79ea939b0ce88e1b1
Michael Swan <swanyboy4@gmail.com>
2009-10-03 21:57:16 -0400

    Update

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+document.onselectstart=new Function('return false');
+function ds(e){return false;}
+function ra(){return true;}
+document.onmousedown=ds;
+document.onclick=ra;
