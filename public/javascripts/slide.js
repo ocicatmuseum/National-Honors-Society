7fcecc168af10ddb3b24a6537527c4868141f6e5
Michael Swan <swanyboy4@gmail.com>
2009-10-13 17:17:41 -0400

    update

--- public/javascripts/slide.js
+++ public/javascripts/slide.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 	
 	// Collapse Panel
 	$("#cover").click(function(){
-		$("div#panel").fadeIn("slow");	
+		$("#cover").animate({width: "800px"}, 1500);	
 	});
 		
 	// Switch buttons from "Log In | Register" to "Close Panel" on click
