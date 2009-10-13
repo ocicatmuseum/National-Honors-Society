7198c6d7b69b3b8ea994b92f54f8d16752e8e98e
Michael Swan <swanyboy4@gmail.com>
2009-10-13 18:32:53 -0400

    update

--- public/javascripts/slide.js
+++ public/javascripts/slide.js
@@ -13,8 +13,7 @@ $(document).ready(function() {
 	
 	// Collapse Panel
 	$("#cover").click(function(){
-		$("#calendar").addClass("fix");
-		$("#calendar").animate({width: "1200px", height: "800px", left: "50px", top: "50px", display: "block", position: "fixed"}, 1500);	
+		$("#calendar").appear(1500);	
 	});
 		
 	// Switch buttons from "Log In | Register" to "Close Panel" on click
