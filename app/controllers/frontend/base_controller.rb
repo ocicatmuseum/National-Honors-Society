602f46afc6fd1de1d359bc55d12006bc18ab34b2
Michael Swan <swanyboy4@gmail.com>
2009-10-16 21:37:49 -0400

    Just changing words.

--- app/controllers/frontend/base_controller.rb
+++ app/controllers/frontend/base_controller.rb
@@ -10,10 +10,15 @@ class Frontend::BaseController < FrontendController
 
   data_2 = LineHollow.new(2,5,'#000099')
   data_2.key("Current Hours",10)
-
-  (0..8).each do |i|
-    data_1.add_data_tip(rand(5) + 14, "(Extra: #{i})")
-    data_2.add_data_tip(rand(5) + 8,  "(Extra: #{i})")
+  last1 = 0
+  last2 = 0
+  (0..5).each do |i|
+    value1 = rand(5) + 14
+    value2 = rand(5) + 8
+    data_1.add_data_tip(value1, value1 - last1)
+    data_2.add_data_tip(value2,  value2 - last2)
+    last1 = value1
+    last2 = value2
   end
   
   g = Graph.new
@@ -22,7 +27,7 @@ class Frontend::BaseController < FrontendController
   g.data_sets << data_2
 
   g.set_tool_tip('#x_label# [#val#]<br>#tip#')
-  g.set_x_labels(%w(10/4 10/11 10/18 10/25 11/1 11/8 11/15 11/22 11/29))
+  g.set_x_labels(%w(10/4 10/11 10/18 10/25 11/1 11/8))
   g.set_x_label_style(10, '#000000', 0, 1)
 
   g.set_y_max(20)
